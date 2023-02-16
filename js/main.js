7483416e435fc4baed3c15f9df3d5d4e8d8439a8
Luiz Henrique Weiss <sistemas2@macromaq.com.br>
2023-02-16 13:44:19 -0300

    Adicionando alguns ajustes no desafio 3

--- js/main.js
+++ js/main.js
@@ -55,32 +55,34 @@ if (gostaLinguagem == 2) {
 */
 
 //Exercício dia 3
-
-const area = prompt("Qual área deseja seguir? Front-End ou Back-End?");
-var especialidade = "";
-
-if (area == 'Front-End' || area == 'Front End' || area == 'FrontEnd') {
-  var especialidade = prompt("Deseja se especializar em React ou Vue?");
-}
-
-if (area == 'Back-End' || area == 'Back End' || area == 'BackEnd') {
-  var especialidade = prompt("Deseja se especializar em C# ou Java?");
-}
-
-const msgEsp = prompt(`Legal! Você escolheu: ` + especialidade + `. Deseja seguir se especializando na área escolhida? (Digite 1). Ou seguir se desenvolvendo para se tornar Fullstack? (Digite 2).`);
-
-if (msgEsp == 1) {
-  alert("Parabéns! Você inicialmente escolheu seguir a área " + area + ", se especializando em " + especialidade + ".");
-}
-
-if (msgEsp == 2) {
-  alert("Você escolheu seguir a carreira Fullstack.");
-
-  var tecnologias = prompt("Quais tecnologias gostaria de se especializar ou conhecer?");
-  var maisTecnologias = prompt("Gostaria de aprender mais alguma tecnologia? S ou N?");
-  while (maisTecnologias == "S") {
-    tecnologias = `${tecnologias}, ${prompt("Quais tecnologias gostaria de se especializar ou conhecer?")}`;
-    var maisTecnologias = prompt("Gostaria de aprender mais alguma tecnologia? S ou N?");
+function iniciarTeste() {
+  const area = prompt("Qual área deseja seguir? 1 - Front-End, ou 2 - Back-End?");
+  var especialidade = "";
+  
+  if (area == 1) {
+    var especialidade = prompt("Deseja se especializar em Angular, React ou Vue?");
   }
-  alert("Parabéns! Você inicialmente escolheu seguir a área " + area + ", se especializando em " + especialidade + ", mas também deseja se tornar um programador Fullstack, conhecendo: " + tecnologias);
+  
+  if (area == 2) {
+    var especialidade = prompt("Deseja se especializar em C# ou Java?");
+  }
+  
+  const msgEsp = prompt(`Legal! Você escolheu: ` + especialidade + `. Deseja seguir se especializando na área escolhida? (Digite 1). Ou seguir se desenvolvendo para se tornar Fullstack? (Digite 2).`);
+  
+  if (msgEsp == 1) {
+    alert("Parabéns! Você inicialmente escolheu seguir a área " + area + ", se especializando em " + especialidade + ".");
+  }
+  
+  if (msgEsp == 2) {
+    alert("Você escolheu seguir a carreira Fullstack.");
+  
+    var tecnologias = prompt("Quais tecnologias gostaria de se especializar ou conhecer?");
+    var maisTecnologias = prompt("Gostaria de aprender mais alguma tecnologia? S ou N?");
+    while (maisTecnologias == "S" || maisTecnologias == "s") {
+      tecnologias = `${tecnologias}, ${prompt("Quais tecnologias gostaria de se especializar ou conhecer?")}`;
+      var maisTecnologias = prompt("Gostaria de aprender mais alguma tecnologia? S ou N?");
+    }
+    alert("Parabéns! Você inicialmente escolheu seguir a área " + area + ", se especializando em " + especialidade + ", mas também deseja se tornar um programador Fullstack, e estudar/conhecer: " + tecnologias + ".");
+  }
+
 }
