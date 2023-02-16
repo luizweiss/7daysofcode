e171a83509ed56a2f9d52ba4801f2940b9efc2e3
Luiz Henrique Weiss <sistemas2@macromaq.com.br>
2023-02-16 13:52:43 -0300

    Últimos ajustes no desafio 3

--- js/main.js
+++ js/main.js
@@ -28,33 +28,36 @@ if (numeroDez == stringDez) {
 
 //Exercício dia 2 solucionado
 /*
-const nome = prompt("Qual o seu nome?");
-const idade = prompt("Quantos anos você tem?");
-const linguagem = prompt("Qual linguagem de programação você está estudando?");
+function iniciarTeste() {
+  const nome = prompt("Qual o seu nome?");
+  const idade = prompt("Quantos anos você tem?");
+  const linguagem = prompt("Qual linguagem de programação você está estudando?");
 
-const msgInicial = `Seu nome é ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`;
+  const msgInicial = `Seu nome é ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`;
 
-alert(msgInicial);
+  alert(msgInicial);
 
-var gostaLinguagem = prompt("Você gosta de estudar " + linguagem + "? Responda com o número 1 para SIM ou 2 para NÃO.");
+  var gostaLinguagem = prompt("Você gosta de estudar " + linguagem + "? Responda com o número 1 para SIM ou 2 para NÃO.");
 
-const msgFinal1 = `Muito bom! Continue estudando e você terá muito sucesso.`;
-const msgFinal2 = `Ahh que pena... Já tentou aprender outras linguagens?`;
+  const msgFinal1 = `Muito bom! Continue estudando e você terá muito sucesso.`;
+  const msgFinal2 = `Ahh que pena... Já tentou aprender outras linguagens?`;
 
-if (gostaLinguagem != 1 && gostaLinguagem != 2) {
-  gostaLinguagem = prompt("Responda com o número 1 para SIM ou 2 para NÃO.");
-}
+  if (gostaLinguagem != 1 && gostaLinguagem != 2) {
+    gostaLinguagem = prompt("Responda com o número 1 para SIM ou 2 para NÃO.");
+  }
 
-if (gostaLinguagem == 1) {
-  alert(msgFinal1);
-}
+  if (gostaLinguagem == 1) {
+    alert(msgFinal1);
+  }
 
-if (gostaLinguagem == 2) {
-  alert(msgFinal2);
+  if (gostaLinguagem == 2) {
+    alert(msgFinal2);
+  }
 }
 */
 
 //Exercício dia 3
+
 function iniciarTeste() {
   const area = prompt("Qual área deseja seguir? 1 - Front-End, ou 2 - Back-End?");
   var especialidade = "";
@@ -67,10 +70,10 @@ function iniciarTeste() {
     var especialidade = prompt("Deseja se especializar em C# ou Java?");
   }
   
-  const msgEsp = prompt(`Legal! Você escolheu: ` + especialidade + `. Deseja seguir se especializando na área escolhida? (Digite 1). Ou seguir se desenvolvendo para se tornar Fullstack? (Digite 2).`);
+  const msgEsp = prompt(`Legal! Você escolheu: ${especialidade}. Deseja seguir se especializando na área escolhida? (Digite 1). Ou seguir se desenvolvendo para se tornar Fullstack? (Digite 2).`);
   
   if (msgEsp == 1) {
-    alert("Parabéns! Você inicialmente escolheu seguir a área " + area + ", se especializando em " + especialidade + ".");
+    alert(`Parabéns! Você inicialmente escolheu seguir a área ${area}, se especializando em ${especialidade}.`);
   }
   
   if (msgEsp == 2) {
@@ -82,7 +85,6 @@ function iniciarTeste() {
       tecnologias = `${tecnologias}, ${prompt("Quais tecnologias gostaria de se especializar ou conhecer?")}`;
       var maisTecnologias = prompt("Gostaria de aprender mais alguma tecnologia? S ou N?");
     }
-    alert("Parabéns! Você inicialmente escolheu seguir a área " + area + ", se especializando em " + especialidade + ", mas também deseja se tornar um programador Fullstack, e estudar/conhecer: " + tecnologias + ".");
+    alert(`Parabéns! Você inicialmente escolheu seguir a área ${area}, se especializando em ${especialidade}, mas também deseja se tornar um programador Fullstack, e estudar/conhecer: ${tecnologias}.`);
   }
-
 }
